d549c76937ef9fd7b3bc0c76bb30b53cdb94017d
atsundwn <atsundwn@gmail.com>
2015-10-20 09:35:02 -0400

    Completed assessment

--- challenge.rb
+++ challenge.rb
@@ -7,6 +7,7 @@
 "Hello World"
 33
 65.89
+# Write .class after it
 
 # Question 2: Booleans
 # What would be the result of executing the below statements
@@ -17,18 +18,26 @@ else
   puts "it's false"
 end
 
+# Since 33 and 55 are both 'truthy' "it's true" will be printed.
+
 # 2.1 Substitue "hello" && 4.5 for 33 && 55 above. What's the output?
+# Same thing both of those also are "truthy"
 # 2.2 Substitue nil && 88 for 33 && 55 above. What's the output?
+# "it's false" because nil is "falsy"
 # 2.4 Substitue 63.4 < 64. What's the output?
+# "it's true" because the conditional is true
 
 # Question 3 :Strings
 
 # How would you (Yes, you can look up the String methods!):
 
 # 3.1. determine if a string is included another another string?
+# string.include?('target string')
 # 3.2. determine if the length of a string?
+# string.length
 # 3.3. Go from the string "eels in my hovercraft" to "fish in my sink" but don't
 # change the string "eels in my hovercraft"
+# string.gsub("eels in my hovercraft", "fish in my sink")
 
 
 # Question 4 : Variables
@@ -38,9 +47,12 @@ end
 vehicle = "4 wheel Bus"
 
 # 4.1 to "6 wheel Bus"
+# vehicle.gsub("4", "6")
 # 4.2 to "6 wheel tank"
+# vehicle.gsub("4", "6").gsub("Bus", "tank")
 # 4.3 Create a NEW string from the string in 4.2 that will be
 # "6 wheel tank that shoots rainbow bunnnies"
+# vehicle.gsub!("4 wheel Bus", "6 wheel tank that shoots rainbow bunnies")
 
 # Question 5 : Loop
 # - write a loop that guesses your age.
@@ -50,6 +62,24 @@ vehicle = "4 wheel Bus"
 # - aaaah maaan c'mon, huh, WTF
 # - I'm not kidding, say unkind works to a user that thinks your 10 years older
 #   than you are.
+age = 33
+guess = nil
+max_attempt = 4
+attempt = 1
+
+while age != guess
+  print "Guess my age!"
+  guess = gets.chomp.to_i
+  unless attempt < max_attempt
+    puts "No more attempts left!"
+    break
+  attempts += 1
+end
+puts "You guessed right!" if guess == age
+end
+
+
+
 
 # Bonus Question: Who said this?
 
